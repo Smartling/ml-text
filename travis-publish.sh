8a1f007b4a259c67556218ad3ac1e64b7af8ddad
Pavel Ivashkov <pavels.code@gmail.com>
2015-12-04 15:14:27 +0200

    ~ signing props from env

--- travis-publish.sh
+++ travis-publish.sh
@@ -2,4 +2,8 @@
 
 openssl aes-256-cbc -k "$SIGNING_PASSWORD" -in .secring.gpg.enc -out .secring.gpg -d
 
-./gradlew -Psigning.secretKeyRingFile=.secring.gpg uploadArchives --info --stacktrace
+./gradlew \
+  -Psigning.keyId="$SIGNING_KEY" \
+  -Psigning.password="$SIGNING_PASSWORD" \
+  -Psigning.secretKeyRingFile="$TRAVIS_BUILD_DIR/.secring.gpg" \
+  uploadArchives --info --stacktrace
